8a12acc00cc8f364b59396db60bbbac0afd87156
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2015-04-29 01:30:23 +0200

    Download Consul template

--- showcase/bin/fetch-dist.sh
+++ showcase/bin/fetch-dist.sh
@@ -12,4 +12,13 @@ else
     wget https://dl.bintray.com/mitchellh/consul/0.5.0_linux_386.zip -O exchange/consul.zip
 fi
 
+
+if [ -e exchange/consul-template.zip ]; then
+    echo "There is an existing Consul Template distribution below exchange";
+    echo "Will not fetch it again";
+else 
+    wget https://github.com/hashicorp/consul-template/releases/download/v0.8.0/consul-template_0.8.0_linux_386.tar.gz -O exchange/consul-template.tar.gz
+fi
+
 ( cd exchange ; unzip -u consul.zip )
+( cd exchange ; tar -zxvf consul-template.tar.gz )
